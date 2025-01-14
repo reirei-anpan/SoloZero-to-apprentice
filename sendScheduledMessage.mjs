82ef7b0628b6f211494660a7ea901254ae76362a
Glitch (minipotato-bot-by-eoi) <none>
2025-01-14 07:04:48 +0000

    ⛈💰 Checkpoint ./database.json:73591300/3543 ./sendScheduledMessage.mjs:73591300/3

--- sendScheduledMessage.mjs
+++ sendScheduledMessage.mjs
@@ -26,7 +26,7 @@ export async function sendScheduledMessage(client) {
       5: "🚨 最後の募集 🚨\n\n応募期限は「本日 23:50分」まで!!\n\n今週の土曜日、21時から同期と交流したいアプレンティス生は「参加」をクリック!!", // 金曜日
     };
 
-    const today = new Date().getDay(); // 今日の曜日を取得 (0: 日曜日, 1: 月曜日, 2:火曜日 ...)
+    const today = 1//new Date().getDay(); // 今日の曜日を取得 (0: 日曜日, 1: 月曜日, 2:火曜日 ...)
     const message =
       messagesByDay[today] || "募集メッセージが設定されていません。";
 
