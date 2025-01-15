432d155b35cb1e9bdeabd8a75605dbc3b090f317
Glitch (minipotato-bot-by-eoi) <none>
2025-01-15 11:59:13 +0000

    🧀⛈ Checkpoint ./deleteCommands.mjs:73591300/3108

--- deleteCommands.mjs
+++ deleteCommands.mjs
@@ -1,20 +1,17 @@
 import { REST } from "@discordjs/rest";
 import { Routes } from "discord-api-types/v9";
 
-const BOT_TOKEN = process.env.TOKEN; // Botのトークン
+const BOT_TOKEN = process.env.TOKEN;
 const CLIENT_ID = "1327916182770290720";
-const GUILD_ID = "1191988459179614228";
 
 const rest = new REST({ version: "9" }).setToken(BOT_TOKEN);
 
 (async () => {
   try {
-    // サーバー内のすべてのスラッシュコマンドを削除
-    await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
-      body: [],
-    });
-    console.log("サーバー内のスラッシュコマンドを削除しました！");
+    // グローバルコマンドを削除
+    await rest.put(Routes.applicationCommands(CLIENT_ID), { body: [] });
+    console.log("グローバルコマンドを削除しました！");
   } catch (error) {
-    console.error("スラッシュコマンド削除中にエラーが発生しました:", error);
+    console.error("グローバルコマンド削除中にエラーが発生しました:", error);
   }
 })();
